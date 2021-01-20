6665451d24e88c22cb5edb110920be86c631cbd6
Pierre <pierre.kieffer@hubstairs.com>
2021-01-20 17:13:23 +0100

    update install scrits

--- install/install_pitop32.sh
+++ install/install_pitop32.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl https://github.com/PierreKieffer/pitop/blob/master/bin/32/pitop --output pitop
+wget https://github.com/PierreKieffer/pitop/raw/master/bin/32/pitop
 
 chmod +x pitop
 sudo mv pitop /usr/local/bin

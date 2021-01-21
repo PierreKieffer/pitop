93f3a9ec2db5bdbbd3d90b4407a5e4fb00d9e0de
Pierre <pierre.kieffer@hubstairs.com>
2021-01-21 10:02:52 +0100

    update install scripts

--- install/install_pitop32.sh
+++ install/install_pitop32.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+echo " ---------- Downloading pitop ---------- "
 wget https://github.com/PierreKieffer/pitop/raw/master/bin/32/pitop
 
 chmod +x pitop
 sudo mv pitop /usr/local/bin
+echo " ---------- pitop is installed ---------- "
+echo " ---------- usage ---------- "
+echo "    start cmd : pitop "
+echo "    press q to exit "
+echo " --------------------------- "
+
+
 
--- install/install_pitop64.sh
+++ install/install_pitop64.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+echo " ---------- Downloading pitop ---------- "
 wget https://github.com/PierreKieffer/pitop/raw/master/bin/64/pitop 
 chmod +x pitop
 sudo mv pitop /usr/local/bin
+echo " ---------- pitop is installed ---------- "
+echo " ---------- usage ---------- "
+echo "    start cmd : pitop "
+echo "    press q to exit "
+echo " --------------------------- "
 
